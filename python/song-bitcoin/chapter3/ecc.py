6c882200143b732d26d787857d8fad42bdee329f
jamillambert <Jamil.Lambert@proton.me>
2022-07-20 16:43:06 +0100

    CH2 Exercise 3

--- python/song-bitcoin/chapter3/ecc.py
+++ python/song-bitcoin/chapter3/ecc.py
@@ -288,12 +288,15 @@ class ECCTest(TestCase):
             (47, 71, 117, 141, 60, 139),
             (143, 98, 76, 66, 47, 71),
         )
-
+        for x1, y1, x2, y2, x3, y3 in additions:
+            p1 = Point(FieldElement(x1, prime), FieldElement(y1, prime), a, b)
+            p2 = Point(FieldElement(x2, prime), FieldElement(y2, prime), a, b)
+            p3 = Point(FieldElement(x3, prime), FieldElement(y3, prime), a, b)
+            self.assertEqual(p1 + p2, p3)
         # loop over additions
         # initialize x's and y's as FieldElements
         # create p1, p2 and p3 as Points
         # check p1+p2==p3
-        raise NotImplementedError
 
     def test_rmul(self):
         # tests the following scalar multiplications
