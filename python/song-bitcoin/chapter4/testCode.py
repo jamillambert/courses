2eb21b969c9eba17dc0b0822ca7ad6133f78ed76
jamillambert <Jamil.Lambert@proton.me>
2022-07-22 12:11:32 +0100

    Ch4 Ex4

--- python/song-bitcoin/chapter4/testCode.py
+++ python/song-bitcoin/chapter4/testCode.py
@@ -1,2 +1,18 @@
-b = b'\x80'
-print(b)
+def decorator1(func):
+    a = a+"I am in wrapper1"
+    return func(a)
+
+
+def decorator2(func):
+    """Comment"""
+    a = a+" and now in wrapper 2"
+    return func(a)
+
+@decorator1
+@decorator2
+def func(a):
+    print(a)
+
+
+a = ""
+func(a)
