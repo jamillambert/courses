40b447c4a745fcec9458ad3b892cfa0088a870fb
jamillambert <Jamil.Lambert@proton.me>
2022-07-27 10:21:14 +0100

    helper.py line 20 ripemd160 changed to rmd160

--- python/song-bitcoin/chapter7/helper.py
+++ python/song-bitcoin/chapter7/helper.py
@@ -17,7 +17,7 @@ def run(test):
 
 def hash160(s):
     '''sha256 followed by ripemd160'''
-    return hashlib.new('ripemd160', hashlib.sha256(s).digest()).digest()
+    return hashlib.new('rmd160', hashlib.sha256(s).digest()).digest()
 
 
 def hash256(s):
@@ -56,7 +56,7 @@ def decode_base58(s):
     combined = num.to_bytes(25, byteorder='big')  # <2>
     checksum = combined[-4:]
     if hash256(combined[:-4])[:4] != checksum:
-        raise ValueError('bad address: {} {}'.format(checksum, 
+        raise ValueError('bad address: {} {}'.format(checksum,
           hash256(combined[:-4])[:4]))
     return combined[1:-4]  # <3>
 # end::source1[]
