5e7c1f279d61673de17f3ced6b92d2941448e41c
jamillambert <Jamil.Lambert@proton.me>
2022-08-05 16:42:48 +0100

    Ch9 Ex13

--- python/song-bitcoin/chapter9/helper.py
+++ python/song-bitcoin/chapter9/helper.py
@@ -151,12 +151,13 @@ def target_to_bits(target):
 def calculate_new_bits(previous_bits, time_differential):
     '''Calculates the new bits given
     a 2016-block time differential and the previous bits'''
-    # if the time differential is greater than 8 weeks, set to 8 weeks
-    # if the time differential is less than half a week, set to half a week
-    # the new target is the previous target * time differential / two weeks
-    # if the new target is bigger than MAX_TARGET, set to MAX_TARGET
-    # convert the new target to bits
-    raise NotImplementedError
+    if time_differential > TWO_WEEKS * 4:
+        time_differential = TWO_WEEKS * 4 # if the differential > 8 weeks, set to 8 weeks
+    elif time_differential < TWO_WEEKS // 4:
+        time_differential = TWO_WEEKS // 4 # if the differential < 1/2 week, set to 1/2 week
+    new_target = bits_to_target(previous_bits) * time_differential // TWO_WEEKS # new target is last target * differential / 2 weeks
+    return target_to_bits(new_target) # convert new target to bits# if the time differential is greater than 8 weeks, set to 8 weeks
+
 
 
 class HelperTest(TestCase):
