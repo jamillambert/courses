94e046b68169d0968a695b03f1f8eeb304dafa6b
jamillambert <Jamil.Lambert@proton.me>
2022-09-06 13:58:21 +0100

    adding gitignore for target directory

---
+++ Rust/Ultimate-Rust-Crash-Course/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/jamil/build/github.com/jamillambert/courses/Rust/Ultimate-Rust-Crash-Course/hello/target/debug/hello: /home/jamil/build/github.com/jamillambert/courses/Rust/Ultimate-Rust-Crash-Course/hello/src/main.rs
