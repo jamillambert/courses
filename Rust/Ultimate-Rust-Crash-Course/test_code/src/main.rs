a829372c7841ee8a4a897a8e0f70ea57fccc1249
jamillambert <Jamil.Lambert@proton.me>
2022-09-07 16:02:19 +0100

    test code

--- Rust/Ultimate-Rust-Crash-Course/test_code/src/main.rs
+++ Rust/Ultimate-Rust-Crash-Course/test_code/src/main.rs
@@ -1,11 +1,12 @@
 
 fn main() {
-    let x = String::from("test string");
+    let mut x = String::from("test string");
     println!("main {}", x);
-    print_test(&x);
+    print_test(&mut x);
     println!("after test {}", x);
 }
 
-fn print_test(s: &String) {
+fn print_test(s: &mut String) {
     println!("test {}", s);
+    *s = String::from("Run once");
 }
