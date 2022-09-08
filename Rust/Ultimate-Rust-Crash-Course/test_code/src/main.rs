f9ae9d9b7cea5e026a7fde4b6a1c4708b4946e2e
jamillambert <Jamil.Lambert@proton.me>
2022-09-08 09:50:53 +0100

    assigning match to a variable

--- Rust/Ultimate-Rust-Crash-Course/test_code/src/main.rs
+++ Rust/Ultimate-Rust-Crash-Course/test_code/src/main.rs
@@ -2,16 +2,17 @@
 fn main() {
         let args: Vec<String> = std::env::args().skip(1).collect();
         for fruit in args {
-            match fruit.as_str() {
+            let message = match fruit.as_str() {
                 "apple" => {
-                    println!("Fruit {} is an apple", fruit);
+                    format!("Fruit {} is an apple", fruit)
                 }
                 "pear" => {
-                    println!("Fruit {} is a pear", fruit);
+                    format!("Fruit {} is a pear", fruit)
                 }
                 _ => {
-                    println!("Fruit {} is not an apple or a pear", fruit);
+                    format!("Fruit {} is not an apple or a pear", fruit)
                 }
-            }
+            };
+            println!("{}", message);
         }
     }
