81f92ffedf9b57a6e1b1381720c77c29d86ef2b7
jamillambert <Jamil.Lambert@proton.me>
2022-09-15 16:00:53 +0100

    exercise testing done

--- Rust/Ultimate_Rust_2/exercise/testing/src/lib.rs
+++ Rust/Ultimate_Rust_2/exercise/testing/src/lib.rs
@@ -12,8 +12,9 @@ pub fn splish(a: i32, b: i32) -> i32 {
 }
 
 // 1. Use the `cfg` attribute to mark the `test` module below as a test module
-
+#[cfg(test)]
 mod test {
+    use super::*;
     // 2. Bring all the library items into scope with a `use` statement
     // Hint: It's okay to use `*` here.
 
@@ -26,10 +27,24 @@ mod test {
     // `cargo test` should run your tests and pass
     // Hint: Don't forget the `#[test]` attribute for your test function!
 
+    #[test]
+    fn test_condions() {
+        assert_eq!(sploosh(1, 2, 3), 4);
+        assert_ne!(sploosh(5, 6, 7), 4);
+        assert_eq!(sploosh(-1, 2, 3), 99);
+    }
+
     // 4. Write a test function that verifies the following conditions using the `assert!` macro
     // - splish(100, 10) is negative
     // - splish(40, 20) is positive
     // - splish(9, 3) is 0
+
+    #[test]
+    fn test_condition_2() {
+        assert!(splish(100, 10) < 0);
+        assert!(splish(40, 29) > 0);
+        assert!(splish(9, 3) == 0);
+    }
 }
 
 // 5. Create a `tests/` directory and an integration test file `tests/more_tests.rs`
