9e9eca38b44a0012d937ba986c029b4f8c5b90f6
jamillambert <Jamil.Lambert@proton.me>
2022-09-16 15:27:46 +0100

    added struct GameSate

--- Rust/rust_game/src/main.rs
+++ Rust/rust_game/src/main.rs
@@ -1,7 +1,15 @@
 use rusty_engine::prelude::*;
 
 struct GameState {
+    name: String,
     health_left: i32,
+    high_score: i32,
+    current_score: i32,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer,
+}
+
+impl Default for GameState {
 }
 
 fn main() {
