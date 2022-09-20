9e15e301c7020d4771fa0114e479761d7191b110
jamillambert <Jamil.Lambert@proton.me>
2022-09-20 11:41:46 +0100

    displays fps

--- Rust/rust_game/src/main.rs
+++ Rust/rust_game/src/main.rs
@@ -1,4 +1,5 @@
-use rusty_engine::prelude::{bevy::prelude::default, *};
+use rusty_engine::{prelude::{bevy::prelude::default, *}, game};
+use std::time::SystemTime;
 
 struct GameState {
     name: String,
@@ -7,6 +8,7 @@ struct GameState {
     current_score: i32,
     enemy_labels: Vec<String>,
     spawn_timer: Timer,
+    measure_time: SystemTime,
 }
 
 impl Default for GameState {
@@ -18,16 +20,23 @@ impl Default for GameState {
             current_score: 0,
             enemy_labels: Vec::new(),
             spawn_timer: Timer::from_seconds(1.0, false),
+            measure_time: SystemTime::now(),
         }
     }
 }
 
 fn main() {
     let mut game = Game::new();
+    game.add_logic(game_logic);
     game.run(GameState::default());
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    if game_state.current_score % 60 == 0 {
+       let new_time = SystemTime::now();
+        let fps = 60000 / SystemTime::duration_since(&new_time, game_state.measure_time).unwrap().as_millis();
+        game_state.measure_time = new_time;
+        println!("Current score: {}, fps: {}", game_state.current_score, fps);
+    }
     game_state.current_score += 1;
-    println!("Current score: {}", game_state.current_score);
 }
