3adb786039d2c005ba68d4c57f6365bdeb779339
jamillambert <Jamil.Lambert@proton.me>
2022-09-21 17:11:14 +0100

    test code

--- Rust/Ultimate-Rust-Crash-Course/test_code/src/main.rs
+++ Rust/Ultimate-Rust-Crash-Course/test_code/src/main.rs
@@ -1,17 +1,14 @@
 fn main() {
-    let args: Vec<String> = std::env::args().skip(1).collect();
-    for fruit in args {
-        let message = match fruit.as_str() {
-            "apple" => {
-                format!("Fruit {} is an apple", fruit)
-            }
-            "pear" => {
-                format!("Fruit {} is a pear", fruit)
-            }
-            _ => {
-                format!("Fruit {} is not an apple or a pear", fruit)
-            }
-        };
-        println!("{}", message);
-    }
+let mut string = String::from("initial"); 
+foo(&mut string);
+println!("{}", string);
+}
+
+fn foo(string: &mut String) {
+    *string += " x";
+    barr(string);
+}
+
+fn barr(string: &mut String) {
+    *string += " x";
 }
