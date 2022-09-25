b4404c2594937ca736dc21e8f0ebb1d6720347be
jamil.lambert <jamil.lambert@proton.me>
2022-09-25 16:44:17 +0100

    benchmarking test

--- Rust/Ultimate_Rust_2/exercise/testing/benches/test_benchmark.rs
+++ Rust/Ultimate_Rust_2/exercise/testing/benches/test_benchmark.rs
@@ -1,9 +1,12 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use testing::sploosh;
+use testing::{sploosh, fizz_buzz};
 
 pub fn test_benchmark(c: &mut Criterion) {
-    c.bench_function("sploosh", |b| {
-        b.iter(|| sploosh(black_box(8), black_box(9), black_box(10)))
+    // c.bench_function("sploosh", |b| {
+    //     b.iter(|| sploosh(black_box(3), black_box(5), black_box(100)))
+    // });
+    c.bench_function("fizz_buzz", |b| {
+        b.iter(|| fizz_buzz(3, 5, 100))
     });
 }
 
