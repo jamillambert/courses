a8724d61332433bf40ec89f6426ee734cfc3e8c1
jamillambert <Jamil.Lambert@proton.me>
2022-09-26 13:20:48 +0100

    Start of Udemy 100 days of python

---
+++ python/100-days-of-code/main.py
@@ -0,0 +1,8 @@
+from time import pthread_getcpuclockid
+
+
+print("What city did you grow up in?") 
+name = input()
+print("What is your pets name?")
+pet = input()
+print("Your band name could be: " + name + " " + pet)
