e788a4dc2634a5a6fbd4e67a36e44a0a298a737b
jamil.lambert <jamil.lambert@proton.me>
2022-09-27 11:48:52 +0100

    test

--- python/100-days-of-code/main.py
+++ python/100-days-of-code/main.py
@@ -1,3 +1,7 @@
+
+import math
+
+
 while True:
     year = input("Enter the year to check if it is a leap year (x to exit): ")
     if year == 'x':
@@ -6,3 +10,5 @@ while True:
         print(f'{year} is a leap year')
     else:
         print(f'{year} is not a leap year')
+pi = math.pi
+print(f'Print format test of pi to 3 decimals = {pi:.3f}')
