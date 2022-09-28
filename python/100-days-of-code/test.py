9b0b32732c0761582f8d71649f68e3d9649c3118
jamil.lambert <jamil.lambert@proton.me>
2022-09-28 11:23:18 +0100

    Day 5 password generator

---
+++ python/100-days-of-code/test.py
@@ -0,0 +1,9 @@
+# File for testing small sections of code that are not required as part of a project
+
+list = [-1, -200, 299, 399]
+max = ''
+
+for i in list:
+    if i > max:
+        max = i
+print(max)
