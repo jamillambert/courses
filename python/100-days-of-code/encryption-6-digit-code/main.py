4958b3a3c0d7a222cfcc74290fb1ce674be38a76
jamil.lambert <jamil.lambert@proton.me>
2022-09-29 16:18:58 +0100

    Comments updated

--- python/100-days-of-code/encryption-6-digit-code/main.py
+++ python/100-days-of-code/encryption-6-digit-code/main.py
@@ -1,7 +1,7 @@
 '''
 A plain text encoder/decoder that takes a 6 digit code as the encoding key
-the character shift is changed after each letter using finite field maths
-spaces and special characters are used to greatly enhance the security over 
+the character shift is changed after each letter using finite field maths.
+Spaces and special characters are used to greatly enhance the security over 
 the simple letter substitution used in the original caesar cypher.
 
 Jamil Lamber 2022
