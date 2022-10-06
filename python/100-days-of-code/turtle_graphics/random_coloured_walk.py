9e8c658381481ac7477119a5fb9ea73229ab5333
jamil.lambert <jamil.lambert@proton.me>
2022-10-06 20:47:43 +0100

    commented the code

--- python/100-days-of-code/turtle_graphics/random_coloured_walk.py
+++ python/100-days-of-code/turtle_graphics/random_coloured_walk.py
@@ -3,11 +3,12 @@ import random
 from turtle import Turtle, Screen
 
 
-def random_walk(steps, step_length, turtles):
+def random_walk(num_steps, step_length, turtles):
+    """Moves the turtles in the input list {num_steps} random steps of up to {step_length} max length"""
     colour = [255, 255, 255]
     colour_sign = [1, 1, 1]
     colour_step = 20
-    for _ in range(steps):
+    for _ in range(num_steps):
         (colour, colour_sign) = random_colour(colour, colour_sign, colour_step)
         for turtle in turtles:
             turtle.color(colour)
@@ -19,6 +20,7 @@ def random_walk(steps, step_length, turtles):
 
 
 def random_position(turtle):
+    """Sets the turtle to a random position on the screen"""
     turtle.up()
     x = int(random.randint(-screen1.window_width() / 2, screen1.window_width() / 2))
     y = int(random.randint(-screen1.window_height() / 2, screen1.window_height() / 2))
@@ -27,6 +29,12 @@ def random_position(turtle):
 
 
 def random_colour(colour, colour_sign, step):
+    """Returns two lists, a list with the colour [r, g, b] and which direction each are moving +/- [1, 1, 1]
+
+    The red green and blue values of the colour are incremented randomly up to the
+    amount in the input step value.  If the value goes out of range it is set to the limit
+    and the direction in colour_sign is changed so it moves in the opposite direction
+    next time"""
     for i in range(3):
         colour[i] += random.randint(0, step) * colour_sign[i]
         if colour[i] > 255:
@@ -39,9 +47,10 @@ def random_colour(colour, colour_sign, step):
 
 
 def multiple_turtle_walk():
+    """Creates a list of turtles and then runs the random_walk on it"""
+    number_turtles = 100
     steps = 2000
     max_step_length = 20
-    number_turtles = 100
     turtle_list = []
     for _ in range(number_turtles):
         turtle = Turtle()
@@ -56,6 +65,7 @@ def multiple_turtle_walk():
     random_walk(steps, max_step_length, turtle_list)
 
 
+# Creates a screen and then runs the animation
 screen1 = Screen()
 screen1.colormode(255)
 screen1.delay(0)
