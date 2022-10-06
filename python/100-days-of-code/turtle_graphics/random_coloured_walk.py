66202a984532031001a06351bcdb87160b44ed6b
jamil.lambert <jamil.lambert@proton.me>
2022-10-06 19:16:37 +0100

    colour changes gradually and pos resets randomly

--- python/100-days-of-code/turtle_graphics/random_coloured_walk.py
+++ python/100-days-of-code/turtle_graphics/random_coloured_walk.py
@@ -6,19 +6,47 @@ from turtle import Turtle, Screen
 def random_walk(distance):
     turtle.down()
     turtle.width(10)
+    r = 0
+    g = 0
+    b = 0
+    r_sign = 1
+    g_sign = 1
+    b_sign = 1
     for _ in range(distance):
         turtle.forward(random.randint(0, 100))
         turtle.right(random.randint(0, 359))
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
+        r += random.randint(0, 20)*r_sign
+        g += random.randint(0, 20)*g_sign
+        b += random.randint(0, 20)*b_sign
+        if r > 255:
+            r_sign = -1
+            r = 255
+        if g > 255:
+            g_sign = -1
+            g = 255
+        if b > 255:
+            b_sign = -1
+            b = 255
+        if r < 0:
+            r_sign = 1
+            r = 0
+        if g < 0:
+            g_sign = 1
+            g = 0
+        if b < 0:
+            b_sign = 1
+            b = 0
         colour = (r, g, b)
         turtle.pencolor(colour)
         if abs(turtle.xcor()) > screen1.window_width()/2 or abs(turtle.ycor()) > screen1.window_height()/2:
-            turtle.goto(0, 0)
+            turtle.up()
+            x = int(random.randint(-screen1.window_width()/2, screen1.window_width()/2))
+            y = int(random.randint(-screen1.window_height()/2, screen1.window_height()/2))
+            turtle.goto(x, y)
+            turtle.down()
 
 
-dist= 1000
+dist = 1000
 turtle = Turtle()
 turtle.speed(10)
 screen1 = Screen()
