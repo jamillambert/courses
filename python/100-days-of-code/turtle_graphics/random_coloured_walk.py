89ccd1b3ffe87e73ed121c40279c2e5b7f793378
jamil.lambert <jamil.lambert@proton.me>
2022-10-06 17:20:02 +0100

    turtle graphics, random coloured walk

---
+++ python/100-days-of-code/turtle_graphics/random_coloured_walk.py
@@ -0,0 +1,28 @@
+from math import sqrt
+import random
+from turtle import Turtle, Screen
+
+
+def random_walk(distance):
+    turtle.down()
+    turtle.width(10)
+    for _ in range(distance):
+        turtle.forward(random.randint(0, 100))
+        turtle.right(random.randint(0, 359))
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        colour = (r, g, b)
+        turtle.pencolor(colour)
+        if abs(turtle.xcor()) > screen1.window_width()/2 or abs(turtle.ycor()) > screen1.window_height()/2:
+            turtle.goto(0, 0)
+
+
+distance = 1000
+turtle = Turtle()
+turtle.speed(10)
+screen1 = Screen()
+screen1.colormode(255)
+random_walk(distance)
+screen1.exitonclick()
+
