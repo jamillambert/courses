f7aa17575ae4825e5b9f39f0aec85ed88b65b94f
jamil.lambert <jamil.lambert@proton.me>
2022-10-06 20:39:52 +0100

    increased speed and changed shape to same colour circle

--- python/100-days-of-code/turtle_graphics/random_coloured_walk.py
+++ python/100-days-of-code/turtle_graphics/random_coloured_walk.py
@@ -3,56 +3,61 @@ import random
 from turtle import Turtle, Screen
 
 
-def random_walk(distance, turtles):
-    r = 0
-    g = 0
-    b = 0
-    r_sign = 1
-    g_sign = 1
-    b_sign = 1
-    for _ in range(distance):
+def random_walk(steps, step_length, turtles):
+    colour = [255, 255, 255]
+    colour_sign = [1, 1, 1]
+    colour_step = 20
+    for _ in range(steps):
+        (colour, colour_sign) = random_colour(colour, colour_sign, colour_step)
         for turtle in turtles:
-            turtle.right(random.randint(0, 359))
-            turtle.forward(random.randint(0, 100))
-            r += random.randint(0, 20)*r_sign
-            g += random.randint(0, 20)*g_sign
-            b += random.randint(0, 20)*b_sign
-            if r > 255:
-                r_sign = -1
-                r = 255
-            if g > 255:
-                g_sign = -1
-                g = 255
-            if b > 255:
-                b_sign = -1
-                b = 255
-            if r < 0:
-                r_sign = 1
-                r = 0
-            if g < 0:
-                g_sign = 1
-                g = 0
-            if b < 0:
-                b_sign = 1
-                b = 0
-            colour = (r, g, b)
+            turtle.color(colour)
             turtle.pencolor(colour)
+            turtle.right(random.randint(0, 359))
+            turtle.forward(random.randint(0, step_length))
             if abs(turtle.xcor()) > screen1.window_width()/2 or abs(turtle.ycor()) > screen1.window_height()/2:
-                turtle.up()
-                x = int(random.randint(-screen1.window_width()/2, screen1.window_width()/2))
-                y = int(random.randint(-screen1.window_height()/2, screen1.window_height()/2))
-                turtle.goto(x, y)
-                turtle.down()
+                random_position(turtle)
 
 
-dist = 1000
-turtle_list = [Turtle(), Turtle(), Turtle(), Turtle(), Turtle(), Turtle()]
-for t in turtle_list:
-    t.speed(10)
-    t.down()
-    t.width(10)
+def random_position(turtle):
+    turtle.up()
+    x = int(random.randint(-screen1.window_width() / 2, screen1.window_width() / 2))
+    y = int(random.randint(-screen1.window_height() / 2, screen1.window_height() / 2))
+    turtle.goto(x, y)
+    turtle.down()
+
+
+def random_colour(colour, colour_sign, step):
+    for i in range(3):
+        colour[i] += random.randint(0, step) * colour_sign[i]
+        if colour[i] > 255:
+            colour_sign[i] = -1
+            colour[i] = 255
+        if colour[i] < 0:
+            colour_sign[i] = 1
+            colour[i] = 0
+    return colour, colour_sign
+
+
+def multiple_turtle_walk():
+    steps = 2000
+    max_step_length = 20
+    number_turtles = 100
+    turtle_list = []
+    for _ in range(number_turtles):
+        turtle = Turtle()
+        turtle.color("white")
+        turtle.shape("circle")
+        turtle.speed(0)
+        turtle.down()
+        turtle.shapesize(0.5,0.5,1)
+        turtle.width(10)
+        random_position(turtle)
+        turtle_list.append(turtle)
+    random_walk(steps, max_step_length, turtle_list)
+
+
 screen1 = Screen()
 screen1.colormode(255)
-random_walk(dist, turtle_list)
+screen1.delay(0)
+multiple_turtle_walk()
 screen1.exitonclick()
-
