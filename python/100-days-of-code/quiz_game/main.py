f1fd995201f5b656ac7cd1b7ac80fb68cf0aa3af
jamillambert <Jamil.Lambert@proton.me>
2022-10-06 12:45:34 +0100

    removed example code

--- python/100-days-of-code/quiz_game/main.py
+++ python/100-days-of-code/quiz_game/main.py
@@ -5,23 +5,24 @@ from os import system
 
 def input_number(text, min, max, default):
     """Returns a number input from the terminal or default if it fails
-    
+
     The input text is displayed to the terminal as a prompt for the user to input
     a number.  If a number is not entered within the input min and max values the
     default value is returned"""
     try:
         number = int(input(text))
     except:
-        number = min -1 # forces if statement below to be true
+        number = min - 1  # forces if statement below to be true
     if number < min or number > max:
-        input(f"invalid input, set to default {default}, press enter to continue")
+        input(
+            f"invalid input, set to default {default}, press enter to continue")
         number = default
     return number
 
 
 def input_text(text, default):
     """Returns either the text in the default array if it is entered or it's index, otherwise the text at index 0 is returned
-    
+
     The input text is displayed to the terminal as a prompt for the user to input
     the index or text of the array."""
     return_text = 'invalid'
@@ -31,11 +32,12 @@ def input_text(text, default):
         return_text = default[number]
     except:
         for d in default:
-            if choice == d: 
+            if choice == d:
                 return_text = d
     if return_text == 'invalid':
         return_text = default[0]
-        input(f"invalid input, set to default {return_text}, press enter to continue")
+        input(
+            f"invalid input, set to default {return_text}, press enter to continue")
     return return_text
 
 
@@ -47,8 +49,8 @@ def set_defaults(quiz):
     quiz.question_type = ''
 
 
-def main_menu(quiz): 
-    """Displays a settings menu where the user can change the quiz settings"""    
+def main_menu(quiz):
+    """Displays a settings menu where the user can change the quiz settings"""
     options = {'n': 'Change the number of questions', 'd': 'Change the difficulty', 't': 'Change the question type',
                's': 'Change the question subject', 'r': 'Reset to default values', 'x': 'Save and exit settings'}
     choice = 'uninitialised'
@@ -61,11 +63,11 @@ def main_menu(quiz):
             print(f"'{key}': {options[key]}")
         choice = input(":")
         if choice == 'n':
-            quiz.num_questions=input_number(
+            quiz.num_questions = input_number(
                 '\nEnter the number of questions: (1 to 20) ', 1, 20, 10)
         elif choice == 'd':
             quiz.difficulty = input_text('\nEnter the difficulty, 1: easy, 2: medium, 3: hard : ', [
-                                    'easy', 'medium', 'hard'])
+                'easy', 'medium', 'hard'])
         elif choice == 't':
             quiz.question_type = input_text(
                 '\nEnter the question type, 1: True/False, 2: Multiple Choice, 3: All Questions : ', ['boolean', 'multiple', ''])
@@ -93,14 +95,15 @@ def print_options(quiz):
 
 
 def new_game():
-    """Creates a new Quiz object, sets the defaults and prompts the user to change settings or keep defaults"""    
+    """Creates a new Quiz object, sets the defaults and prompts the user to change settings or keep defaults"""
     quiz = Quiz()
     set_defaults(quiz)
     system('cls||clear')
     print(("\n\n\033[1;36mWelcome to the Trivia Quiz\033[0m\n\n"))
     print("Questions are sourced from Open Trivia Database https://opentdb.com/")
     print_options(quiz)
-    choice = input("\nPress Enter to continue, or type 's' to change settings: ")
+    choice = input(
+        "\nPress Enter to continue, or type 's' to change settings: ")
     if choice == 's':
         main_menu(quiz)
     print("Sourcing questions from https://opentdb.com/")
@@ -111,7 +114,7 @@ def new_game():
 def main():
     """Starts a new quiz, when finished the users is asked to start again or exit"""
     while True:
-        quiz = new_game() 
+        quiz = new_game()
 
         system('cls||clear')
         for i in range(quiz.num_questions):
@@ -124,6 +127,6 @@ def main():
             continue
         else:
             break
-        
-        
+
+
 main()
