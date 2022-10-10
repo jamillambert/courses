c0e2db69eb1869874e5954aa16c2d0c22ec4da76
jamil.lambert <jamil.lambert@proton.me>
2022-10-10 17:06:09 +0100

    comment

--- python/100-days-of-code/turtle_graphics/main.py
+++ python/100-days-of-code/turtle_graphics/main.py
@@ -135,7 +135,7 @@ def main():
     screen.tracer(number_turtles, 0)
     turtle_list = multiple_turtles(random_start, number_turtles, width, height)
 
-    """Spirograph"""
+    # """Spirograph"""
     # try:
     #     spirograph(separation, size, turtle_list)
     #     print("Animation finished")
@@ -143,7 +143,7 @@ def main():
     # except (turtle.Terminator, _tkinter.TclError):
     #     print("Window was closed before the animation finished")
 
-    """Random walk"""
+    # """Random walk"""
     # try:
     #     random_walk(number_steps, max_step_length, turtle_list, width, height)
     #     print("Animation finished")
