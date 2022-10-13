f88ce6c8738d2d1e18d7627310ec2ac1e9d8f23f
jamil.lambert <jamil.lambert@proton.me>
2022-10-13 20:30:55 +0100

    removed shadowing of variables

--- python/100-days-of-code/test.py
+++ python/100-days-of-code/test.py
@@ -3,7 +3,7 @@ import math
 
 
 def sqrt(n, precision):
-    ans = n
+    answer = n
     max_remainder = 1
     error = n/2.0
     if n < 0:
@@ -12,17 +12,19 @@ def sqrt(n, precision):
     for i in range(precision):
         max_remainder *= 0.1
     while abs(error) > max_remainder:
-        result = ans*ans
+        result = answer * answer
         if error > 0.0 and result < n:
             error = -error/2
         elif error < 0.0 and result > n:
             error = -error/2
-        ans = ans - error
-    return ans
-n = 2
-precision = 10
-ans = sqrt(n, 10)
-math_ans = math.sqrt(n)
-print("Square root of {} is {:0.9f} with a calculation precision of {}".format(n, ans, precision))
-print("Using the math crate the square root of {} is {}".format(n, math_ans))
-print("Difference between the two is {:0.2e}".format(ans-math_ans))
+        answer = answer - error
+    return answer
+
+
+number = 2
+prec = 10
+ans = sqrt(number, 10)
+math_ans = math.sqrt(number)
+print("Square root of {} is {:0.9f} with a calculation precision of {}".format(number, ans, prec))
+print("Using the math crate the square root of {} is {}".format(number, math_ans))
+print("Difference between the two is {:0.2e}".format(ans - math_ans))
