721829340bad89cfe3119873f8cb0ac92b6a692b
jamil.lambert <jamil.lambert@proton.me>
2022-11-08 11:09:54 +0000

    test.py

--- python/100-days-of-code/test.py
+++ python/100-days-of-code/test.py
@@ -1,3 +1,9 @@
 # File for testing small sections of code that are not required as part of a project
-test = 'not as big'
-print(test[0:-1])
+def f(a, L=[]):
+    L.append(a)
+    return L
+
+
+print(f(1))
+print(f(2))
+print(f(3))
