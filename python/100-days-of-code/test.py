5080b595d1c5919ed1c428c555668585bb8abdc3
jamil.lambert <jamil.lambert@proton.me>
2022-11-24 11:22:56 +0000

    removed unnecessary return

--- python/100-days-of-code/test.py
+++ python/100-days-of-code/test.py
@@ -6,33 +6,29 @@ def river_sizes(matrix):
         for col in range(len(matrix[row])):
             if matrix[row][col] == 1:
                 matrix[row][col] = 0
-                matrix, new_river = get_river(matrix, row, col)
+                new_river = get_river(matrix, row, col)
                 rivers.append(new_river)
-    print(rivers)         
+    return rivers        
                 
 def get_river(matrix, row, col):
     river_len = 1
-    if row > 1:
+    if row > 0:
         if matrix[row-1][col] == 1:
             matrix[row-1][col] = 0
-            matrix, extra_length = get_river(matrix, row-1, col)
-            river_len += extra_length
-    if col > 1:
+            river_len += get_river(matrix, row-1, col)
+    if col > 0:
         if matrix[row][col-1] == 1:
             matrix[row][col-1] = 0
-            matrix, extra_length = get_river(matrix, row, col-1)
-            river_len += extra_length
-    if row < len(matrix)-2:
+            river_len += get_river(matrix, row, col-1)
+    if row < len(matrix)-1:
         if matrix[row+1][col] == 1:
             matrix[row+1][col] = 0
-            matrix, extra_length = get_river(matrix, row+1, col)
-            river_len += extra_length
-    if col < len(matrix[0])-2:
+            river_len += get_river(matrix, row+1, col)
+    if col < len(matrix[0])-1:
         if matrix[row][col+1] == 1:
             matrix[row][col+1] = 0
-            matrix, extra_length = get_river(matrix, row, col+1)
-            river_len += extra_length
-    return matrix, river_len
+            river_len += get_river(matrix, row, col+1)
+    return river_len
 
 
 matrix = [
@@ -42,4 +38,4 @@ matrix = [
     [1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0],
     [1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1]]
 
-river_sizes(matrix)
+print(river_sizes(matrix))
