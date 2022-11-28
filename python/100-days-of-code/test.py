d1a6a1442dc9bc35a2a3fcc9f8b5243a520217dc
jamil.lambert <jamil.lambert@proton.me>
2022-11-28 13:14:01 +0000

    algoexpert

--- python/100-days-of-code/test.py
+++ python/100-days-of-code/test.py
@@ -1,2 +1,13 @@
-x = [[1 for a in range(10)] for a in range(10)]
-print(x)
+def threeNumberSum(array, targetSum):
+    # Write your code here.
+    array.sort()
+    triplets = []
+    for i in range(len(array)-2):
+        x = array[i]
+        for j in range(i+1, len(array)-1):
+            y = array[j]
+            for k in range(j+1, len(array)):
+                z = array[k]
+                if x + y + z == targetSum:
+                    triplets.append([x, y, z])
+    return triplets
