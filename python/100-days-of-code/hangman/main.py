c2cb8d99eadef2bb6e3f4bbc4a654340c98981dc
jamil.lambert <jamil.lambert@proton.me>
2022-12-01 15:08:26 +0000

    refactored hangman

--- python/100-days-of-code/hangman/main.py
+++ python/100-days-of-code/hangman/main.py
@@ -16,69 +16,99 @@ from hangman_art import stages, logo
 from hangman_words import word_list
 import random
 
-# A random word from the list stored in hangman_words.py is chosen
-word = random.choice(word_list)
-answer = ['_'] * len(word)
-lives = len(stages) - 1
-previous_choices = ['']
-running_text = "Welcome to hangman, choose your first letter (or type 'exit')\n"
+class GameState:
+    def __init__(self):
+        self.word = random.choice(word_list)
+        self.answer = ['_'] * len(self.word)
+        self.lives = len(stages) - 1
+        self.previous_choices = ['']
+        self.running_text = "Welcome to hangman, choose your first letter (or type 'exit')\n"
 
-while True:
-    # Loops until the player chooses to exit the game
-    os.system('cls||clear')
-    print(logo)
-    print(stages[lives]) # The hangman drawing that adds a section each time a wrong letter is chosen
-    print(' '.join(answer) + "\n")
-    if '_' not in answer or lives == 0:
-        # The game is over and is reset
-        if '_' not in answer:
-            # There are no more blank letters so the player has won
+    def game_over(self):
+        '''Returns true if the game is over'''
+        return '_' not in self.answer or self.lives == 0
+    
+    def game_won(self):  
+        '''Returns True if the player has won'''  
+        if '_' not in self.answer:
+            # No more blanks in the answer so the player has won
             print("\nCongradulations you win!\n")
+            return True
         else:
             # No more lives left so the player has lost
             print("\nSorry you have lost, try again\n")
-            print("The word was: " + word +  "\n")
-        letter = input("Do you want to play again? (y/n) ")
-        if letter == 'y':
-            # A new word is chosen and the answer and live variables are reset
-            word = random.choice(word_list)
-            answer = ['_'] * len(word)
-            lives = len(stages) - 1
-            previous_choices = ['']
-            running_text = "Welcome to hangman, choose your first letter (or type 'exit')\n"
-            continue
-        else:
-            print("\n\nThanks for playing, bye\n\n")
-            break
+            print("The word was: " + self.word +  "\n")
+            return False
     
-    # Display the running text and ask the player to input a letter
-    letter = input(running_text).lower()
-    if letter == 'exit':
-        print("\nSorry you have lost, due to being a quitter\n")
-        print("The word was: " + word + "\n\nBye\n")
-        break
-    elif len(letter) != 1:
-        # A single letter was not input
-        running_text = 'Already chosen letters: ' + ' '.join(previous_choices)
-        running_text += "\n\nInvalid input, enter a single letter (or type 'exit')\n"
-    elif letter in previous_choices: 
-        # The letter was already chosen, no change to answer or lives
-        running_text = 'Already chosen letters: ' + ' '.join(previous_choices)
-        running_text += "\n\nYou have already tried {}, try another one (or type 'exit')\n".format(
-            letter)
-    elif letter in word:
-        # The letter chosen is in the word, it is added to the answer array
-        previous_choices.append(letter)
-        running_text = 'Already chosen letters: ' + ' '.join(previous_choices)
-        running_text += "\n\nGood work, {} is in the word, choose the next letter (or type 'exit')\n".format(
-            letter)
-        for i in range(len(word)):
-           if letter == word[i]:
-               answer[i] = letter
-    else: 
-        # The letter is not in the word, 1 life lost
-        lives -= 1
-        previous_choices.append(letter)
-        running_text = 'Already chosen letters: ' + ' '.join(previous_choices)
-        running_text += "\n\nSorry, {} is in not in the word, try another one (or type 'exit')\n".format(
-            letter)
+    def print_state(self):
+        '''Prints out the logo and current state of the game'''
+        print(logo)
+        print(stages[self.lives]) # The hangman drawing that adds a section each time a wrong letter is chosen
+        print(' '.join(self.answer) + "\n")
+
+    def input_letter(self):
+        '''Prompts player to input a letter and return False if they choose to exit'''
+        letter = input(self.running_text).lower()
+        if letter == 'exit':
+            print("\nSorry you have lost, due to being a quitter\n")
+            print("The word was: " + self.word + "\n\nBye\n")
+            return False
+        elif len(letter) != 1:
+            # A single letter was not input
+            running_text = 'Already chosen letters: ' + ' '.join(self.previous_choices)
+            running_text += "\n\nInvalid input, enter a single letter (or type 'exit')\n"
+        elif letter in self.previous_choices: 
+            # The letter was already chosen, no change to answer or lives
+            running_text = 'Already chosen letters: ' + ' '.join(self.previous_choices)
+            running_text += "\n\nYou have already tried {}, try another one (or type 'exit')\n".format(
+                letter)
+        elif letter in self.word:
+            # The letter chosen is in the word, it is added to the answer array
+            self.previous_choices.append(letter)
+            running_text = 'Already chosen letters: ' + ' '.join(self.previous_choices)
+            running_text += "\n\nGood work, {} is in the word, choose the next letter (or type 'exit')\n".format(
+                letter)
+            for i in range(len(self.word)):
+                if letter == self.word[i]:
+                    self.answer[i] = letter
+        else: 
+            # The letter is not in the word, 1 life lost
+            self.lives -= 1
+            self.previous_choices.append(letter)
+            running_text = 'Already chosen letters: ' + ' '.join(self.previous_choices)
+            running_text += "\n\nSorry, {} is in not in the word, try another one (or type 'exit')\n".format(
+                letter)
+        return True
+
+def main():
+    # A random word from the list stored in hangman_words.py is chosen within game
+    game = GameState()
+    
+    while True:
+        # Loops until the player chooses to exit the game
+        os.system('cls||clear')
+        game.print_state()
+
+        if game.game_over():
+            # The game is over
+            game.game_won()
+            if play_again():
+                # A new game with a new word is created
+                game = GameState()
+                continue
+            else:
+                print("\n\nThanks for playing, bye\n\n")
+                break
+        
+        # Display the running text and ask the player to input a letter
+        if not game.input_letter():
+            # If the player chooses to exit this breaks out of the loop
+            break
+
+
+def play_again():
+    '''Returns True if the player chooses to play again'''
+    letter = input("Do you want to play again? (y/n) ")
+    return letter == 'y'
+
+main()
