efaf8fb8a7340cd4d42c84704cf449d464f90a25
jamillambert <Jamil.Lambert@proton.me>
2022-12-12 15:39:10 +0000

    test code

--- Rust/Ultimate-Rust-Crash-Course/test_code/src/main.rs
+++ Rust/Ultimate-Rust-Crash-Course/test_code/src/main.rs
@@ -1,14 +1,18 @@
 fn main() {
-let mut string = String::from("initial"); 
-foo(&mut string);
-println!("{}", string);
+    let string = String::from("12s3a"); 
+    let number = str_to_int(string);
+    println!("{}", number);
 }
 
-fn foo(string: &mut String) {
-    *string += " x";
-    barr(string);
+fn str_to_int(string: String) -> i32{
+    // Returns an integer from the input string. 
+    // non numerical characters are ignored and 0 is returned if the string contains no digits
+    let mut number = 0;
+    for c in string.chars(){
+        if 0 <= c as i32 - 0x30 && c as i32 - 0x30 <= 9 {
+            number = number * 10 + c as i32 - 0x30;
+        }
+    }
+    return number;
 }
 
-fn barr(string: &mut String) {
-    *string += " x";
-}
