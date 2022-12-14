6340c28115b0c997c06f9735ac70ac8c8c69d1be
jamil.lambert <jamil.lambert@proton.me>
2022-12-14 16:35:50 +0000

    added exit to end of help

--- Rust/cat/src/main.rs
+++ Rust/cat/src/main.rs
@@ -21,11 +21,10 @@ fn cat(file: &String, options: &Vec<char>) {
         }
         if options.contains(&'b') {
             if line.len() != 0 {
-                non_blank_lines +=1;
+                non_blank_lines += 1;
                 print!("\x1b[42m{:>3} \x1b[0m", non_blank_lines);
             }
-        }
-        else if options.contains(&'n') {
+        } else if options.contains(&'n') {
             print!("\x1b[42m{:>3} \x1b[0m", line_no);
         }
         println!("{}", line);
@@ -33,7 +32,7 @@ fn cat(file: &String, options: &Vec<char>) {
 }
 
 fn read_options(arg: &String) -> Vec<char> {
-    // Returns a vector of characters of all implemented options in the input string
+    // Returns a vector of characters of all implemented options input
     let mut options = Vec::new();
     let implemented_options = ['n', 's', 'h', 'b'];
     for char in arg.chars() {
@@ -45,7 +44,7 @@ fn read_options(arg: &String) -> Vec<char> {
 }
 
 fn print_help() {
-    // Prints the help text
+    // Prints the help text and then exits
     println!("CAT written in Rust\n");
     println!("\x1b[1mNAME\x1b[0m");
     println!("\tcat - concatenate files and print on the standard output\n");
@@ -58,17 +57,25 @@ fn print_help() {
     println!("\t-s\tsuppress repeated empty output lines\n");
     println!("\x1b[1mEXAMPLES\x1b[0m");
     println!("\tcat -ns file1.txt file2.txt");
-    println!("\t\toutput file1.txt and then file2.txt contents with line numbering and");
+    println!(
+        "\t\toutput file1.txt and then file2.txt contents with line 
+            numbering and"
+    );
     println!("\t\trepeated empty lines suppressed.\n");
     println!("\x1b[1mAUTHOR\x1b[0m");
     println!("\tWritten by Jamil Lambert\n");
+    std::process::exit(0);
 }
+
 fn main() {
-    // Reads in the options, if -h is used the help is printed and the program exits
-    // otherwise each file in the list is passed to cat() to be output
+    // Reads in the options, if -h is used the help is printed and the program
+    // exits, otherwise each file in the list is passed to cat() to be output
     let args: Vec<String> = env::args().collect();
     let mut options = Vec::new();
     let mut start = 1;
+    if args.len() < 2 {
+        print_help();
+    }
     if args[1].chars().next().unwrap() == '-' {
         options = read_options(&args[1]);
         start = 2;
@@ -82,3 +89,4 @@ fn main() {
         }
     }
 }
+
